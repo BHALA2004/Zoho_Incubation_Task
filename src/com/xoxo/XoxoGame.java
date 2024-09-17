922101ad3d3c5c39954061d83a75abd8a665ec76
BHALA2004 <balakannantvl@gmail.com>
2024-09-18 01:47:41 +0530

    Xo

--- src/com/xoxo/XoxoGame.java
+++ src/com/xoxo/XoxoGame.java
@@ -3,6 +3,13 @@ package com.xoxo;
 import java.util.Scanner;
 
 public class XoxoGame {
+    public static void getInput(char[][] Board,char b,int rowX,int columnX){
+
+        Board[rowX][columnX]=b;
+
+
+    }
+
     public static void print(char[][] Board){
         for (int i = 0;i<Board.length;i++){
             for(int j=0;j<Board[0].length;j++){
@@ -39,68 +46,110 @@ public class XoxoGame {
 
         }}
         else {
-            for (int val = i;val>=Board.length;val--){
-                if(Board[val][val]==b){
+            int row = 0;
+            for (int val = i;val>=0;val--){
+                if(Board[row][val]==b){
                     count++;
                 }
+                row++;
             }
         }
         return count==Board.length?true:false;
     }
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+
+        int count = 0;
         char[][] Board = new char[3][3];
+        boolean[][] check = new boolean[3][3];
+        for(int i = 0;i<check.length;i++){
+            for(int j = 0;j< check.length;j++){
+                check[i][j]=true;
+            }
+        }
         boolean loopcase = true;
-        while (loopcase){
-            System.out.println("Enter row player X:");
-            int rowX = scanner.nextInt()-1;
-            System.out.println("Enter column for player X:");
-            int columnX = scanner.nextInt()-1;
-            Board[rowX][columnX]='X';
-            print(Board);
-            for(int i = 0;i<Board.length;i++){
-                if(rowcheck(Board,i,'X')){
-                    System.out.println("X wins");loopcase=false;System.exit(1);
-                }
-            }
-            for(int i = 0;i<Board.length;i++){
-                if(colcheck(Board,i,'X')){
-                    System.out.println("X wins");loopcase=false;System.exit(1);
-                }
-            }
+        int count1  = 0;
+        char currentPlayer = 'X';
+        while (loopcase) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter row player :");
+            int rowX = scanner.nextInt() - 1;
+            System.out.println("Enter column for player:");
+            int columnX = scanner.nextInt() - 1;
 
-            if(diagonalcheck(Board,0,'X')){
-                System.out.println("X wins");loopcase=false;System.exit(1);
-            }
-            else if(diagonalcheck(Board,Board.length-1,'X')){
-                System.out.println("X wins");loopcase=false;System.exit(1);
-            }
-
-            System.out.println("Enter row  Player O:");
-            int rowO = scanner.nextInt()-1;
-            System.out.println("Enter column for Player O:");
-            int columnO = scanner.nextInt()-1;
-            Board[rowO][columnO]='O';
-            print(Board);
-            for(int i = 0;i<Board.length;i++){
-                if(rowcheck(Board,i,'O')){
-                    System.out.println("O wins");loopcase=false;System.exit(1);
-                }
-            }
-            for(int i = 0;i<Board.length;i++){
-                if(colcheck(Board,i,'O')){
-                    System.out.println("O wins");loopcase=false;System.exit(1);
-                }
-            }
-            if(diagonalcheck(Board,0,'O')){
-                System.out.println("O wins");loopcase=false;System.exit(1);
-            }
-            else if(diagonalcheck(Board,Board.length-1,'O')){
-                System.out.println("O wins");loopcase=false;System.exit(1);
+            // If spot is already taken, skip this iteration and ask for a new input
+            if (!check[rowX][columnX]) {
+                System.out.println("Already Have Value");
+                continue;
             }
 
 
+
+
+
+                getInput(Board,currentPlayer, rowX, columnX);
+                print(Board);count1++;
+                check[rowX][columnX] = false;
+
+                for (int i = 0; i < Board.length; i++) {
+                    if (rowcheck(Board, i, 'X')) {
+                        System.out.println("X wins");
+                        loopcase = false;
+                        System.exit(1);
+                    }
+                }
+
+                for (int i = 0; i < Board.length; i++) {
+                    if (colcheck(Board, i, 'X')) {
+                        System.out.println("X wins");
+                        loopcase = false;
+                        System.exit(1);
+                    }
+                }
+
+                if (diagonalcheck(Board, 0, 'X')) {
+                    System.out.println("X wins");
+                    loopcase = false;
+                    System.exit(1);
+                } else if (diagonalcheck(Board, Board.length - 1, 'X')) {
+                    System.out.println("X wins");
+                    loopcase = false;
+                    System.exit(1);
+                }
+             currentPlayer=currentPlayer=='X'?'O':'X';
+
+
+                for (int i = 0; i < Board.length; i++) {
+                    if (rowcheck(Board, i, 'O')) {
+                        System.out.println("O wins");
+                        loopcase = false;
+                        System.exit(1);
+                    }
+                }
+
+                for (int i = 0; i < Board.length; i++) {
+                    if (colcheck(Board, i, 'O')) {
+                        System.out.println("O wins");
+                        loopcase = false;
+                        System.exit(1);
+                    }
+                }
+
+                if (diagonalcheck(Board, 0, 'O')) {
+                    System.out.println("O wins");
+                    loopcase = false;
+                    System.exit(1);
+                } else if (diagonalcheck(Board, Board.length - 1, 'O')) {
+                    System.out.println("O wins");
+                    loopcase = false;
+                    System.exit(1);
+                }
+
+            if(count1==9){
+                System.out.println("Match Draw");System.exit(1);
+            }
 
         }
+
+
     }
 }
