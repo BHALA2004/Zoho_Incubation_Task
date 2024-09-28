d848107fc30a8779a7a9c26ae490737d4a312d7d
BHALA2004 <balakannantvl@gmail.com>
2024-09-28 20:01:13 +0530

    change

--- src/com/relation/Relation.java
+++ src/com/relation/Relation.java
@@ -7,21 +7,21 @@ public class Relation {
     public static void main(String[] args) {
 
         ArrayList<Member> list = new ArrayList<>();
-        list.add(new Member("Eswaran",null,null,'M'));    // 0
-        list.add(new Member("Mookammal",null,null,'F'));   // 1
+        list.add(new Member("bala's dad's dad",null,null,'M'));    // 0
+        list.add(new Member("bala's dad's mom",null,null,'F'));   // 1
         list.add(new Member("Kannan",list.get(0),list.get(1),'F'));     // 2
 
-        list.add(new Member("Muniyappan",null,null,'M'));//3
-        list.add(new Member("Pandiyammal",null,null,'F'));//4
+        list.add(new Member("bala's mom's dad",null,null,'M'));//3
+        list.add(new Member("bala's mom's dad",null,null,'F'));//4
         list.add(new Member("Thangam",list.get(3),list.get(4),'F'));//5
         list.add(new Member("Bala",list.get(2),list.get(5),'M'));//6
-        list.add(new Member("Aarthi",null,null,'F'));//7
-        list.add(new Member("Vijay",list.get(6),list.get(7),'M'));//8
-        list.add(new Member("Sanjay",list.get(6),list.get(7),'M'));//9
-        list.add(new Member("Samantha",list.get(6),list.get(7),'F'));//10
-        list.add(new Member("Marima",list.get(3),list.get(4),'F'));//11
-        list.add(new Member("Panju",list.get(3),list.get(4),'F'));//12
-        list.add(new Member("Muni Small",list.get(3),list.get(4),'M'));//13
+        list.add(new Member("xxx",null,null,'F'));//7
+        list.add(new Member("yyy",list.get(6),list.get(7),'M'));//8
+        list.add(new Member("zzz",list.get(6),list.get(7),'M'));//9
+        list.add(new Member("vvv",list.get(6),list.get(7),'F'));//10
+        list.add(new Member("bala's mom's sibling 1",list.get(3),list.get(4),'F'));//11
+        list.add(new Member("bala's mom's sibling 2",list.get(3),list.get(4),'F'));//12
+        list.add(new Member("bala's mom's sibling 3",list.get(3),list.get(4),'M'));//13
         list.add(new Member("Arumugam",null,null,'M'));    // 14
         list.add(new Member("Murugan",null,null,'M'));    // 15
         list.add(new Member("Viji",null,null,'F'));    // 16
